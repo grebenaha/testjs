b113b98d8ffb5af6ef3253e1f6a6cad7dd903d94
gerbenaha <grebenakha@gmail.com>
2017-04-01 18:53:02 +0300

    new commit

---
+++ public/javascripts/main.js
@@ -0,0 +1,20 @@
+/**
+ * Created by neiro on 01/04/2017.
+ */
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello World';
+
+// var myVar = [1, 'Igor', 'Grebenaha', 10];
+// console.log(myVar[1] + ' ' + myVar[2]);
+
+// var iceCream = 'chocolate';
+// if(iceCream === 'chocolate') {
+//     alert('Yay, I love chocolate ice cream');
+// } else {
+//     alert('Awwww, but chocolate is my favorite');
+// }
+
+function multiply(num1, num2) {
+    var result = num1 * num2;
+    return result;
+}
